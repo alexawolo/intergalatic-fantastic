4158e9e1f4c556d94b1aa02a7503af386038c5c7
alexawolo <alexawoloszuk@gmail.com>
2023-12-04 15:17:04 -0500

    clean up code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,27 @@
-import { getServerSession } from 'next-auth';
-import { options } from '@/lib/auth';
-import Nav from './Nav';
+'use client';
+import { Container, Flex, Link, Text } from '@chakra-ui/react';
+import UserAccountNav from './UserAccountNav';
+import { useSession } from 'next-auth/react';
 
-const Navbar = async () => {
-  const session = await getServerSession(options)
+const Navbar = () => {
+  const { data: session } = useSession()
   
-  return (
-    <Nav user={session?.user}/>
-  );
+  return <Flex w='100%' top='0' py='2' bg='gray.100' pos='fixed' borderBottom='1px' borderBottomColor='gray'>
+        <Container display='flex' alignItems="center" justifyContent="space-between" m='0' minW='100%'>
+            <Link href='/'>
+            <Text fontSize='2xl'>👽</Text>
+            </Link>
+            {session?.user ? (
+                <Flex alignItems='center'>
+                    <UserAccountNav />
+                </Flex>
+            ) : (
+            <Link colorScheme="black" href='/sign-in'>
+                Sign in
+            </Link>
+            )}
+        </Container>
+    </Flex>
 };
 
 export default Navbar;
